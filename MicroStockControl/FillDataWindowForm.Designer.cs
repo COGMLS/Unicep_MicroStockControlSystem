1d9875d166aaf91c2f66acff1c3f348a37eabbb7
COGMLS <mlsmatheus@hotmail.com>
2022-05-24 21:01:28 -0300

    Added text boxes and aligned all UI elements.

--- MicroStockControl/FillDataWindowForm.Designer.cs
+++ MicroStockControl/FillDataWindowForm.Designer.cs
@@ -38,12 +38,25 @@
 			this.QuantityStockLabel = new System.Windows.Forms.Label();
 			this.IdCodeLabel = new System.Windows.Forms.Label();
 			this.BrandLabel = new System.Windows.Forms.Label();
+			this.ProductTextBox = new System.Windows.Forms.TextBox();
+			this.BrandTextBox = new System.Windows.Forms.TextBox();
+			this.ManufacturerTextBox = new System.Windows.Forms.TextBox();
+			this.LotTextBox = new System.Windows.Forms.TextBox();
+			this.ManufacturingDateTextBox = new System.Windows.Forms.TextBox();
+			this.IdCodeTextBox = new System.Windows.Forms.TextBox();
+			this.QuantityStockTextBox = new System.Windows.Forms.TextBox();
+			this.UnitPriceTextBox = new System.Windows.Forms.TextBox();
+			this.UnitTextBox = new System.Windows.Forms.TextBox();
+			this.ExpirationDateTextBox = new System.Windows.Forms.TextBox();
+			this.SaveButton = new System.Windows.Forms.Button();
+			this.SaveNextButton = new System.Windows.Forms.Button();
+			this.CancelButton = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// ProductLabel
 			// 
 			this.ProductLabel.AutoSize = true;
-			this.ProductLabel.Location = new System.Drawing.Point(12, 9);
+			this.ProductLabel.Location = new System.Drawing.Point(12, 15);
 			this.ProductLabel.Name = "ProductLabel";
 			this.ProductLabel.Size = new System.Drawing.Size(53, 15);
 			this.ProductLabel.TabIndex = 0;
@@ -52,7 +65,7 @@
 			// ManufacturerLabel
 			// 
 			this.ManufacturerLabel.AutoSize = true;
-			this.ManufacturerLabel.Location = new System.Drawing.Point(12, 39);
+			this.ManufacturerLabel.Location = new System.Drawing.Point(12, 73);
 			this.ManufacturerLabel.Name = "ManufacturerLabel";
 			this.ManufacturerLabel.Size = new System.Drawing.Size(65, 15);
 			this.ManufacturerLabel.TabIndex = 1;
@@ -61,7 +74,7 @@
 			// LotLabel
 			// 
 			this.LotLabel.AutoSize = true;
-			this.LotLabel.Location = new System.Drawing.Point(12, 54);
+			this.LotLabel.Location = new System.Drawing.Point(12, 102);
 			this.LotLabel.Name = "LotLabel";
 			this.LotLabel.Size = new System.Drawing.Size(33, 15);
 			this.LotLabel.TabIndex = 2;
@@ -70,7 +83,7 @@
 			// ManufacturingDateLabel
 			// 
 			this.ManufacturingDateLabel.AutoSize = true;
-			this.ManufacturingDateLabel.Location = new System.Drawing.Point(12, 69);
+			this.ManufacturingDateLabel.Location = new System.Drawing.Point(12, 131);
 			this.ManufacturingDateLabel.Name = "ManufacturingDateLabel";
 			this.ManufacturingDateLabel.Size = new System.Drawing.Size(110, 15);
 			this.ManufacturingDateLabel.TabIndex = 3;
@@ -79,7 +92,7 @@
 			// ExpirationDateLabel
 			// 
 			this.ExpirationDateLabel.AutoSize = true;
-			this.ExpirationDateLabel.Location = new System.Drawing.Point(12, 84);
+			this.ExpirationDateLabel.Location = new System.Drawing.Point(12, 160);
 			this.ExpirationDateLabel.Name = "ExpirationDateLabel";
 			this.ExpirationDateLabel.Size = new System.Drawing.Size(97, 15);
 			this.ExpirationDateLabel.TabIndex = 4;
@@ -88,7 +101,7 @@
 			// UnitLabel
 			// 
 			this.UnitLabel.AutoSize = true;
-			this.UnitLabel.Location = new System.Drawing.Point(12, 99);
+			this.UnitLabel.Location = new System.Drawing.Point(12, 189);
 			this.UnitLabel.Name = "UnitLabel";
 			this.UnitLabel.Size = new System.Drawing.Size(54, 15);
 			this.UnitLabel.TabIndex = 5;
@@ -97,7 +110,7 @@
 			// UnitPriceLabel
 			// 
 			this.UnitPriceLabel.AutoSize = true;
-			this.UnitPriceLabel.Location = new System.Drawing.Point(12, 114);
+			this.UnitPriceLabel.Location = new System.Drawing.Point(12, 218);
 			this.UnitPriceLabel.Name = "UnitPriceLabel";
 			this.UnitPriceLabel.Size = new System.Drawing.Size(85, 15);
 			this.UnitPriceLabel.TabIndex = 6;
@@ -106,7 +119,7 @@
 			// QuantityStockLabel
 			// 
 			this.QuantityStockLabel.AutoSize = true;
-			this.QuantityStockLabel.Location = new System.Drawing.Point(12, 129);
+			this.QuantityStockLabel.Location = new System.Drawing.Point(12, 247);
 			this.QuantityStockLabel.Name = "QuantityStockLabel";
 			this.QuantityStockLabel.Size = new System.Drawing.Size(137, 15);
 			this.QuantityStockLabel.TabIndex = 7;
@@ -115,7 +128,7 @@
 			// IdCodeLabel
 			// 
 			this.IdCodeLabel.AutoSize = true;
-			this.IdCodeLabel.Location = new System.Drawing.Point(12, 144);
+			this.IdCodeLabel.Location = new System.Drawing.Point(12, 276);
 			this.IdCodeLabel.Name = "IdCodeLabel";
 			this.IdCodeLabel.Size = new System.Drawing.Size(148, 15);
 			this.IdCodeLabel.TabIndex = 8;
@@ -124,17 +137,127 @@
 			// BrandLabel
 			// 
 			this.BrandLabel.AutoSize = true;
-			this.BrandLabel.Location = new System.Drawing.Point(12, 24);
+			this.BrandLabel.Location = new System.Drawing.Point(12, 44);
 			this.BrandLabel.Name = "BrandLabel";
 			this.BrandLabel.Size = new System.Drawing.Size(43, 15);
 			this.BrandLabel.TabIndex = 9;
 			this.BrandLabel.Text = "Marca:";
 			// 
+			// ProductTextBox
+			// 
+			this.ProductTextBox.Location = new System.Drawing.Point(164, 12);
+			this.ProductTextBox.Name = "ProductTextBox";
+			this.ProductTextBox.Size = new System.Drawing.Size(624, 23);
+			this.ProductTextBox.TabIndex = 19;
+			// 
+			// BrandTextBox
+			// 
+			this.BrandTextBox.Location = new System.Drawing.Point(164, 41);
+			this.BrandTextBox.Name = "BrandTextBox";
+			this.BrandTextBox.Size = new System.Drawing.Size(624, 23);
+			this.BrandTextBox.TabIndex = 20;
+			// 
+			// ManufacturerTextBox
+			// 
+			this.ManufacturerTextBox.Location = new System.Drawing.Point(164, 70);
+			this.ManufacturerTextBox.Name = "ManufacturerTextBox";
+			this.ManufacturerTextBox.Size = new System.Drawing.Size(624, 23);
+			this.ManufacturerTextBox.TabIndex = 21;
+			// 
+			// LotTextBox
+			// 
+			this.LotTextBox.Location = new System.Drawing.Point(164, 99);
+			this.LotTextBox.Name = "LotTextBox";
+			this.LotTextBox.Size = new System.Drawing.Size(624, 23);
+			this.LotTextBox.TabIndex = 22;
+			// 
+			// ManufacturingDateTextBox
+			// 
+			this.ManufacturingDateTextBox.Location = new System.Drawing.Point(164, 128);
+			this.ManufacturingDateTextBox.Name = "ManufacturingDateTextBox";
+			this.ManufacturingDateTextBox.Size = new System.Drawing.Size(624, 23);
+			this.ManufacturingDateTextBox.TabIndex = 23;
+			// 
+			// IdCodeTextBox
+			// 
+			this.IdCodeTextBox.Location = new System.Drawing.Point(164, 273);
+			this.IdCodeTextBox.Name = "IdCodeTextBox";
+			this.IdCodeTextBox.Size = new System.Drawing.Size(624, 23);
+			this.IdCodeTextBox.TabIndex = 28;
+			// 
+			// QuantityStockTextBox
+			// 
+			this.QuantityStockTextBox.Location = new System.Drawing.Point(164, 244);
+			this.QuantityStockTextBox.Name = "QuantityStockTextBox";
+			this.QuantityStockTextBox.Size = new System.Drawing.Size(624, 23);
+			this.QuantityStockTextBox.TabIndex = 27;
+			// 
+			// UnitPriceTextBox
+			// 
+			this.UnitPriceTextBox.Location = new System.Drawing.Point(164, 215);
+			this.UnitPriceTextBox.Name = "UnitPriceTextBox";
+			this.UnitPriceTextBox.Size = new System.Drawing.Size(624, 23);
+			this.UnitPriceTextBox.TabIndex = 26;
+			// 
+			// UnitTextBox
+			// 
+			this.UnitTextBox.Location = new System.Drawing.Point(164, 186);
+			this.UnitTextBox.Name = "UnitTextBox";
+			this.UnitTextBox.Size = new System.Drawing.Size(624, 23);
+			this.UnitTextBox.TabIndex = 25;
+			// 
+			// ExpirationDateTextBox
+			// 
+			this.ExpirationDateTextBox.Location = new System.Drawing.Point(164, 157);
+			this.ExpirationDateTextBox.Name = "ExpirationDateTextBox";
+			this.ExpirationDateTextBox.Size = new System.Drawing.Size(624, 23);
+			this.ExpirationDateTextBox.TabIndex = 24;
+			// 
+			// SaveButton
+			// 
+			this.SaveButton.Location = new System.Drawing.Point(12, 367);
+			this.SaveButton.Name = "SaveButton";
+			this.SaveButton.Size = new System.Drawing.Size(75, 23);
+			this.SaveButton.TabIndex = 29;
+			this.SaveButton.Text = "Salvar";
+			this.SaveButton.UseVisualStyleBackColor = true;
+			// 
+			// SaveNextButton
+			// 
+			this.SaveNextButton.Location = new System.Drawing.Point(93, 367);
+			this.SaveNextButton.Name = "SaveNextButton";
+			this.SaveNextButton.Size = new System.Drawing.Size(115, 23);
+			this.SaveNextButton.TabIndex = 30;
+			this.SaveNextButton.Text = "Salvar e Próximo";
+			this.SaveNextButton.UseVisualStyleBackColor = true;
+			// 
+			// CancelButton
+			// 
+			this.CancelButton.Location = new System.Drawing.Point(713, 367);
+			this.CancelButton.Name = "CancelButton";
+			this.CancelButton.Size = new System.Drawing.Size(75, 23);
+			this.CancelButton.TabIndex = 31;
+			this.CancelButton.Text = "Cancelar";
+			this.CancelButton.UseVisualStyleBackColor = true;
+			// 
 			// FillDataWindowForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(800, 402);
+			this.Controls.Add(this.CancelButton);
+			this.Controls.Add(this.SaveNextButton);
+			this.Controls.Add(this.SaveButton);
+			this.Controls.Add(this.IdCodeTextBox);
+			this.Controls.Add(this.QuantityStockTextBox);
+			this.Controls.Add(this.UnitPriceTextBox);
+			this.Controls.Add(this.UnitTextBox);
+			this.Controls.Add(this.ExpirationDateTextBox);
+			this.Controls.Add(this.ManufacturingDateTextBox);
+			this.Controls.Add(this.LotTextBox);
+			this.Controls.Add(this.ManufacturerTextBox);
+			this.Controls.Add(this.BrandTextBox);
+			this.Controls.Add(this.ProductTextBox);
 			this.Controls.Add(this.BrandLabel);
 			this.Controls.Add(this.IdCodeLabel);
 			this.Controls.Add(this.QuantityStockLabel);
@@ -145,6 +268,8 @@
 			this.Controls.Add(this.LotLabel);
 			this.Controls.Add(this.ManufacturerLabel);
 			this.Controls.Add(this.ProductLabel);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
 			this.Name = "FillDataWindowForm";
 			this.Text = "Fill Data";
 			this.ResumeLayout(false);
@@ -164,5 +289,18 @@
 		private Label QuantityStockLabel;
 		private Label IdCodeLabel;
 		private Label BrandLabel;
+		private TextBox ProductTextBox;
+		private TextBox BrandTextBox;
+		private TextBox ManufacturerTextBox;
+		private TextBox LotTextBox;
+		private TextBox ManufacturingDateTextBox;
+		private TextBox IdCodeTextBox;
+		private TextBox QuantityStockTextBox;
+		private TextBox UnitPriceTextBox;
+		private TextBox UnitTextBox;
+		private TextBox ExpirationDateTextBox;
+		private Button SaveButton;
+		private Button SaveNextButton;
+		private Button CancelButton;
 	}
 }
