58151d80d6523f9a95db7796c61e633fa131304c
COGMLS <mlsmatheus@hotmail.com>
2022-06-13 12:40:26 -0300

    Updated the form design.

--- MicroStockControl/StockViewForm.Designer.cs
+++ MicroStockControl/StockViewForm.Designer.cs
@@ -28,12 +28,51 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.StockDataGridView = new System.Windows.Forms.DataGridView();
+			this.RefreshDataGridViewButton = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.StockDataGridView)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// StockDataGridView
+			// 
+			this.StockDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.StockDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.StockDataGridView.Location = new System.Drawing.Point(12, 12);
+			this.StockDataGridView.Name = "StockDataGridView";
+			this.StockDataGridView.ReadOnly = true;
+			this.StockDataGridView.RowTemplate.Height = 25;
+			this.StockDataGridView.Size = new System.Drawing.Size(776, 385);
+			this.StockDataGridView.TabIndex = 0;
+			// 
+			// RefreshDataGridViewButton
+			// 
+			this.RefreshDataGridViewButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.RefreshDataGridViewButton.Location = new System.Drawing.Point(12, 415);
+			this.RefreshDataGridViewButton.Name = "RefreshDataGridViewButton";
+			this.RefreshDataGridViewButton.Size = new System.Drawing.Size(75, 23);
+			this.RefreshDataGridViewButton.TabIndex = 1;
+			this.RefreshDataGridViewButton.Text = "Atualizar";
+			this.RefreshDataGridViewButton.UseVisualStyleBackColor = true;
+			// 
+			// StockViewForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.RefreshDataGridViewButton);
+			this.Controls.Add(this.StockDataGridView);
+			this.Name = "StockViewForm";
 			this.Text = "StockViewForm";
+			((System.ComponentModel.ISupportInitialize)(this.StockDataGridView)).EndInit();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private DataGridView StockDataGridView;
+		private Button RefreshDataGridViewButton;
 	}
 }
