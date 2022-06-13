9433e1e777d790d89198d496731e14cdc59096b0
COGMLS <mlsmatheus@hotmail.com>
2022-06-13 15:55:12 -0300

    Created a default form for messages and warnings.

---
+++ MicroStockControl/DefaultMessageForm.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace MicroStockControl
+{
+	partial class DefaultMessageForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.MessageTextBox = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// MessageTextBox
+			// 
+			this.MessageTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.MessageTextBox.Location = new System.Drawing.Point(12, 12);
+			this.MessageTextBox.Multiline = true;
+			this.MessageTextBox.Name = "MessageTextBox";
+			this.MessageTextBox.ReadOnly = true;
+			this.MessageTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.MessageTextBox.Size = new System.Drawing.Size(451, 142);
+			this.MessageTextBox.TabIndex = 0;
+			// 
+			// DefaultMessageForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(477, 211);
+			this.Controls.Add(this.MessageTextBox);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "DefaultMessageForm";
+			this.Text = "DefaultMessageForm";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private TextBox MessageTextBox;
+	}
+}
---
+++ MicroStockControl/DefaultMessageForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MicroStockControl
+{
+	public partial class DefaultMessageForm : Form
+	{
+		public DefaultMessageForm(string Title, string[] Message)
+		{
+			InitializeComponent();
+
+			this.Text = Title;
+
+			string TmpMessage = "";
+
+			if (Message.Length > 1)
+			{
+				for(int i = 0; i < Message.Length; i++)
+				{
+					TmpMessage += Message[i];
+
+					if(i < Message.Length - 1)
+					{
+						TmpMessage += "\n";
+					}
+				}
+			}
+			else
+			{
+				TmpMessage += Message[0];
+			}
+
+			this.MessageTextBox.Text = TmpMessage;
+		}
+	}
+}
